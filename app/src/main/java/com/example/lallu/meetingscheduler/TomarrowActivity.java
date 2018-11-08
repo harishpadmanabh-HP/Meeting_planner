ab0fe514d525002c4020eb5e3f452a82cab52ffd
lalluprakash1995 <lalluprakash.sics@gmail.com>
2018-11-08 15:26:38 +0530

    added a custom Listview

---
+++ app/src/main/java/com/example/lallu/meetingscheduler/TomarrowActivity.java
@@ -0,0 +1,13 @@
+package com.example.lallu.meetingscheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TomarrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tomarrow);
+    }
+}
